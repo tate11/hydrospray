0a09abbe72ef2304dc137bc93c9948e3f9efdb4f
sugapablo <russ@sugapablo.com>
2017-11-06 19:17:58 -0500

    new mod

---
+++ website_custom_css/__manifest__.py
@@ -0,0 +1,9 @@
+ # __manifest__.py
+ {
+     'name': "Website Custom CSS",
+     'description': "Custom CSS Sheet",
+     'category': 'Generic Modules/Others',
+     'depends': ['web'],
+     'data': [],
+     'css': ['static/src/css/website_custom_css.css'],
+ }
