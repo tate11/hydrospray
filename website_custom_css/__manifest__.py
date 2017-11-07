787e5a6c9518d4b664813fc312de7366746c5c4b
sugapablo <russ@sugapablo.com>
2017-11-06 20:01:47 -0500

    removing test dir

--- website_custom_css/__manifest__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-{
-     'name': "Website Custom CSS",
-     'description': "Custom CSS Sheet",
-     'category': 'Generic Modules/Others',
-     'depends': ['web'],
-     'data': [],
-     'css': ['static/src/css/website_custom_css.css'],
- }
