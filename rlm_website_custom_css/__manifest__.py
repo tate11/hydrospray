c5fd628ecc2766fd4ee276ee7ed37247bf0d3703
sugapablo <russ@sugapablo.com>
2017-11-06 20:38:29 -0500

    from scaffold with css 2

--- rlm_website_custom_css/__manifest__.py
+++ rlm_website_custom_css/__manifest__.py
@@ -19,10 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': [
-    	'base',
-    	'web',
-    ],
+    'depends': [],
 
     # always loaded
     'data': [
