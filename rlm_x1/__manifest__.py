9cadf165d56d2e6931572088ead8247a8de23287
sugapablo <russ@sugapablo.com>
2017-11-14 16:10:14 -0500

    Adding rlm_x1 0.1

---
+++ rlm_x1/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "rlm_x1",
+
+    'summary': """
+        Red Lab Media Website Extension 1""",
+
+    'description': """
+        Red Lab Media Odoo Website Extension. Containing custom LESS, Snippets, etc. 
+    """,
+
+    'author': "Russ Schneider",
+    'website': "http://www.redlabmedia.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Website',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['web','web_editor','website'],
+
+    # always loaded
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+        'views/snippets.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
